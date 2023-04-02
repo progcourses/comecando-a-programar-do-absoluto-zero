930ea3b1ceef4c5088b699374488557cd5689d9a
Bruno Henrique <brunoh915@gmail.com>
2023-04-02 19:44:55 -0300

    12 Primeiras APIs get.

--- api-nodejs/index.js
+++ api-nodejs/index.js
@@ -9,4 +9,27 @@ server.listen(8081, function() {
     console.log("Escutando porta 8081");
 });
 
+server.get("/pessoas", function(req, res, next) {
+    var pessoa = {
+        id: 1,
+        nome: "Prog"
+    };
+    console.log(req);
+    console.log("Consultando pessoa no banco de dados");
+    res.send(pessoa);
+});
+
+server.get("/pessoas/:id", function(req, res, next) {
+    var idPessoa = req.params.id;
+
+    var pessoa = {
+        id: idPessoa,
+        nome: "Pessoa" + idPessoa
+    };
+
+    console.log(req);
+    console.log("Consultando pessoa de id " + idPessoa);
+    res.send(pessoa);
+});
+
 console.log("Iniciando servidor.");
